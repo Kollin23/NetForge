9f6f67690ded45056353932411fe83607f03a778
Kollin23 <carlosdelgadocampos231205@gmail.com>
2025-10-21 11:56:42 +0200

    EOF modificados por EOM

--- cgi-bin/model_cgi.sh
+++ cgi-bin/model_cgi.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 echo "Content-type: text/html; charset=UTF-8"
 echo ""
-cat <<EOF
+/bin/cat << EOM
 <html>
 <head><title>Modelo CGI</title></head>
 <body>
 <h1>Modelo CGI Ejecutado</h1>
 <p>Este es un ejemplo de un script CGI en Bash.</p>
-</body>
-</html>
-EOF
+EOM
 
 echo "<b>"
 ls -l
 echo "</b>"
+
+/bin/cat << EOM
+</body>
+</html>
+EOM
