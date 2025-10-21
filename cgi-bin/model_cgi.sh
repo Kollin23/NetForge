0d02fea2094cd03ebce306ca43b2b88135d570b4
Kollin23 <carlosdelgadocampos231205@gmail.com>
2025-10-21 11:51:47 +0200

    modificado con EOF y ls -l añadido

--- cgi-bin/model_cgi.sh
+++ cgi-bin/model_cgi.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 echo "Content-type: text/html; charset=UTF-8"
 echo ""
-echo "<html>"
-echo "<head><title>Modelo CGI</title></head>"
-echo "<body>"
-echo "<h1>Modelo CGI Ejecutado</h1>"
-echo "<p>Este es un ejemplo de un script CGI en Bash.</p>"
-echo "</body>"
-echo "</html>"
+cat <<EOF
+<html>
+<head><title>Modelo CGI</title></head>
+<body>
+<h1>Modelo CGI Ejecutado</h1>
+<p>Este es un ejemplo de un script CGI en Bash.</p>
+</body>
+</html>
+EOF
+
+ls -l
