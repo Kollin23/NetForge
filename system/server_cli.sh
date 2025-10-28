f27c7ff9fe1777cb2a6a96bc4df223e4d7788982
Kollin23 <carlosdelgadocampos231205@gmail.com>
2025-10-28 09:43:28 +0100

    conexion servidor multiple solucionado

--- system/server_cli.sh
+++ system/server_cli.sh
@@ -9,5 +9,14 @@ rm -f "$FIFO"
 mkfifo "$FIFO"
 chmod 660 "$FIFO"
 
-cat /tmp/f | /usr/local/NetForge/system/script_cli.sh 2>&1 | nc -lk 127.0.0.1 1234 > /tmp/f
+# Función para manejar la conexión y leer/escribir del FIFO
+handle_connection() {
+  cat "$FIFO" | /usr/local/NetForge/system/script_cli.sh 2>&1 | nc -l 127.0.0.1 1234 > "$FIFO"
+}
 
+# Ejecutar servidor nc continuamente
+while true; do
+    # Asegurarse de que el servidor se mantenga activo y reiniciar la conexión cuando sea necesario
+    handle_connection
+    echo "Esperando nueva conexión..."
+done
