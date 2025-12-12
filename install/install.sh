5f4fb09eeb8a7f2a1036e3a056571b492270f9b9
Kollin23 <carlosdelgadocampos231205@gmail.com>
2025-12-12 13:30:25 +0100

    configurar todo en antigravity

--- install/install.sh
+++ install/install.sh
@@ -8,16 +8,15 @@ apt upgrade -y
 apt install -y apache2
 a2enmod cgi
 service apache2 restart
-apt install net-tools
+apt install -y curl
+apt install -y net-tools
 apt install -y expect
+apt install -y iw
 
 #################################################################################
 ### Desactivar NetworkManager
 #################################################################################
 echo "Desactivando NetworkManager"
-
 systemctl stop NetworkManager
 systemctl disable NetworkManager
-echo 
-
-
+echo
